b69412f9aa2a3d59c1657e95270788a352df2ab2
Mitchell <90817905+SharmaMitchell@users.noreply.github.com>
2022-02-07 22:36:46 +0000

    Split officerList into a separate js file (to clean up index.html)

---
+++ officerList.js
@@ -0,0 +1,42 @@
+var officerList = [
+	{
+	"name": "Officer Name",
+	"role": "Officer Role",
+	"image": "./img/placeholder.png",
+	},
+	{
+	"name": "Officer Name",
+	"role": "Officer Role",
+	"image": "./img/placeholder.png",
+	},
+	{
+	"name": "Officer Name",
+	"role": "Officer Role",
+	"image": "./img/placeholder.png",
+	},
+	{
+	"name": "Officer Name",
+	"role": "Officer Role",
+	"image": "./img/placeholder.png",
+	},
+	{
+	"name": "Officer Name",
+	"role": "Officer Role",
+	"image": "./img/placeholder.png",
+	},
+	{
+	"name": "Officer Name",
+	"role": "Officer Role",
+	"image": "./img/placeholder.png",
+	},
+	{
+	"name": "Officer Name",
+	"role": "Officer Role",
+	"image": "./img/placeholder.png",
+	},
+	{
+	"name": "Officer Name",
+	"role": "Officer Role",
+	"image": "./img/placeholder.png",
+	},
+];
