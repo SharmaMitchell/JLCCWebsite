92dae698c43354f32a39f50dcaaec55b35a6f02a
Mitchell <90817905+SharmaMitchell@users.noreply.github.com>
2022-08-26 21:23:38 +0000

    Updates

--- officerList.js
+++ officerList.js
@@ -39,4 +39,9 @@ var officerList = [
 	"role": "Officer Role",
 	"image": "./img/placeholder.png",
 	},
+	{
+	"name": "Officer Name",
+	"role": "Officer Role",
+	"image": "./img/placeholder.png",
+	},
 ];
