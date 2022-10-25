d5253f293eb0f90420e3c0249cf064d5fffc27f9
Mitchell <90817905+SharmaMitchell@users.noreply.github.com>
2022-10-25 00:07:13 +0000

    Improved event cards' image scaling, added dropdown with event description and URLs.

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger'
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 const header = document.querySelector('.header.container');
+var event_item = document.querySelectorAll('.events .events-bottom .events-item');
+var event_button = document.querySelectorAll('.events .events-bottom .events-item .events-info .events-info-expand');
 
 hamburger.addEventListener('click', () => {
 	hamburger.classList.toggle('active');
@@ -22,4 +24,35 @@ menu_item.forEach((item) => {
 		hamburger.classList.toggle('active');
 		mobile_menu.classList.toggle('active');
 	});
+});
+
+console.log(event_item.length)
+
+function getEventItem(){
+	event_item = document.querySelectorAll('.events .events-bottom .events-item');
+	event_button = document.querySelectorAll('.events .events-bottom .events-item .events-info .events-info-expand');
+	if(event_item.length === 0){
+		setTimeout(getEventItem, 250);
+	}
+	else{
+		event_item.forEach((event) => {
+			event.addEventListener('click', () => {
+				event.querySelector('.events-info.more').classList.toggle('active');
+				// mobile_menu.classList.toggle('active');
+			});
+		});
+	}
+}
+
+if(event_item.length === 0){ 
+	getEventItem();
+}
+let x = 0;
+event_item.forEach((event) => {
+	event.addEventListener('click', () => {
+		event.querySelector('.events-info.more').classList.toggle('active');
+		event_button[x].classList.toggle('active');
+		// mobile_menu.classList.toggle('active');
+	});
+	x++;
 });
